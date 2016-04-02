08f062eb8071b7991f8e63399b37ed59cf838ad8
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2016-04-02 18:08:18 -0500

    Initial Commit

---
+++ src/Win32Interop.WinHandles.Tests/TopLevelWindowUtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Win32Interop.WinHandles.Tests
+{
+  public class TopLevelWindowUtilsTests
+  {
+    [Test]
+    public void ListAllWindows()
+    {
+      var allWindows = TopLevelWindowUtils.FindWindows(w => w.GetWindowText() != "");
+
+      Console.WriteLine("All the windows that are currently present:");
+
+      foreach (var windowHandle in allWindows)
+      {
+        Console.WriteLine(windowHandle.GetWindowText());
+      }
+    }
+
+    [Test]
+    public void ShowForeground()
+    {
+      var windowHandle = TopLevelWindowUtils.GetForegroundWindow();
+      Console.WriteLine(windowHandle.GetWindowText());
+    }
+  }
+}
---
+++ src/Win32Interop.WinHandles/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Win32Interop.WinHandles")]
+[assembly: AssemblyDescription("Library for interacting with Win32 window")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinHandle")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("632f72c0-0d20-48d3-b432-59f1c647b55d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
